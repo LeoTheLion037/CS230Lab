e800c2ea57fd7bb9881cafc93a830287df3f3c9e
Leo Peele <leonardopeele@gmail.com>
2023-10-30 22:54:23 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section 003</h2>
+      <p>800401981</p>
+      <p>Hi I am Leo</p>
+    </div>
+  );
+}
+
+export default App;
